c0087f11d0adfc052f14d461d20fe8a0cbbf0c4f
TonyJM <53893112+TonyJM@users.noreply.github.com>
2019-10-10 12:22:32 +0530

    Add files via upload

---
+++ A6_Brackets.py
@@ -0,0 +1,39 @@
+class Balance:
+
+    def __init__(self):
+        self.inp=''
+
+    def get(self):
+        self.inp=input('\nEnter string : ') 
+    
+    def check(self):
+        x=self.inp
+        s=[]
+        openB=['(','{','[']
+        closeB=[')','}',']']
+        for i in x:
+            if i in openB:
+                s.append(i)
+            elif i in closeB:
+                if len(s)>0:
+                    k=closeB.index(i)
+                    if s.pop()!=openB[k]:
+                        return False
+                else:
+                    return False
+        if len(s)>0:
+            return False
+        return True
+
+    def disp(self):
+        res=self.check()
+        if res==True:
+            print('Valid')
+        else:
+            print('Invalid')
+
+
+a=Balance()
+for i in range(3):
+    a.get()
+    a.disp()
